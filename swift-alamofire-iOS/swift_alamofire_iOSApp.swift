17d6174cbc71ccc45ace56f25626ca659679cc72
luoerming <me@luoerming.com>
2021-06-21 15:40:19 +0800

    Initial Commit

---
+++ swift-alamofire-iOS/swift_alamofire_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_alamofire_iOSApp.swift
+//  swift-alamofire-iOS
+//
+//  Created by carroll on 2021/6/21.
+//
+
+import SwiftUI
+
+@main
+struct swift_alamofire_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
